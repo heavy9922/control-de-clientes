8d07025a1eb4d66b8d627d832d1af6d5d419b559
Yeferson Castiblanco <yeferson@freelanders.co>
2021-07-04 23:33:58 -0500

    add directory componets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { TableComponent } from './component/table/table.component';
+import { ClientsComponent } from './component/clients/clients.component';
+import { EditComponent } from './component/edit/edit.component';
+import { LoginComponent } from './component/login/login.component';
+import { RegisterComponent } from './component/register/register.component';
+import { ConfigComponent } from './component/config/config.component';
+import { NoFoundComponent } from './component/no-found/no-found.component';
+import { FoodPageComponent } from './component/food-page/food-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    TableComponent,
+    ClientsComponent,
+    EditComponent,
+    LoginComponent,
+    RegisterComponent,
+    ConfigComponent,
+    NoFoundComponent,
+    FoodPageComponent
   ],
   imports: [
     BrowserModule,
